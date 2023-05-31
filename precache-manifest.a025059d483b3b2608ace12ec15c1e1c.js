4296af7d9b9b01a696c58d723a876ce0caa4ff12
masumahmedeesha <masumahmedeesha@gmail.com>
2023-05-31 10:50:48 +0600

    Updates

---
+++ precache-manifest.a025059d483b3b2608ace12ec15c1e1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acaed31cb4d629c6f8f0fcf37f4e1988",
+    "url": "/intent-detection/index.html"
+  },
+  {
+    "revision": "c4b69eb24c7378a8eaf7",
+    "url": "/intent-detection/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d8b89f17a3fd0392045a",
+    "url": "/intent-detection/static/js/2.021d89b7.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/intent-detection/static/js/2.021d89b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b69eb24c7378a8eaf7",
+    "url": "/intent-detection/static/js/main.13d06d1b.chunk.js"
+  },
+  {
+    "revision": "960543b3dcb75a609750",
+    "url": "/intent-detection/static/js/runtime-main.fecc0c99.js"
+  }
+]);
